b798513c1c2fa6c06ae3bf8d3126281a25c8a047
Giulio Celidoni <giulio.celidoni@gmail.com>
2023-10-27 22:06:37 +0200

    Feat: test4

--- files/Scripts/startup.sh
+++ files/Scripts/startup.sh
@@ -6,5 +6,9 @@ python3 /prepare.py
 # Point to the correct directory
 cd /ut-server/System
 
+# Set correct executables
+chmod +x ./ucc-bin
+chmod +x ./ucc-bin-arm64
+
 # Start server
 ./ucc-bin server $UT_SERVERURL ini=UnrealTournament.ini log=ut.log -nohomedir -lanplay
